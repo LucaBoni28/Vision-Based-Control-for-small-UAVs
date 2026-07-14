20ba763616c195dcbd93310ac7ff6e06c40712e6
LucaBoni28 <boninsegna.luca28@gmail.com>
2026-07-14 12:16:18 +0200

    change velo sending to mission

--- classes/flight_controller.py
+++ classes/flight_controller.py
@@ -114,21 +114,11 @@ class FlightController:
             self._last_vel_log = current_time
 
         if self.master:
-            # Temporarily masquerade as the Autopilot so MAVProxy/Mission Planner doesn't filter it out
-            original_sys = self.master.mav.srcSystem
-            original_comp = self.master.mav.srcComponent
-            self.master.mav.srcSystem = self.target_system
-            self.master.mav.srcComponent = self.target_component
-            
             time_boot_ms = int(current_time * 1000) % 4294967295
             self.master.mav.named_value_float_send(time_boot_ms, b'CmdVx', vx)
             self.master.mav.named_value_float_send(time_boot_ms, b'CmdVy', vy)
             self.master.mav.named_value_float_send(time_boot_ms, b'CmdVz', vz)
             self.master.mav.named_value_float_send(time_boot_ms, b'CmdYawR', yaw_rate)
-            
-            # Restore original system and component ID
-            self.master.mav.srcSystem = original_sys
-            self.master.mav.srcComponent = original_comp
 
         self.master.mav.set_position_target_local_ned_send(
             0, self.target_system, self.target_component,
